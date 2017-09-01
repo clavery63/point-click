3993135530317d19055b72c1df2b043701c199b4
clavery63 <clavery63@gmail.com>
2017-09-01 19:09:29 -0400

    move initial state into its own module

--- src/index.jsx
+++ src/index.jsx
@@ -2,58 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import initialState from './initialState.js';
 import reducers from './reducers.js';
 import App from './app.jsx';
 
-const store = createStore(reducers, {
-  items: [
-    'torch',
-    'key1',
-    'pot roast',
-    'key2',
-    'key3'
-  ],
-  ui: {
-    page: 0
-  },
-  room: {
-    items: [
-      {
-        style: {
-          position: 'absolute',
-          left: 100,
-          top: 100,
-          width: 50,
-          height: 50,
-          'background-color': 'yellow'
-        },
-        name: 'bong'
-      },
-      {
-        style: {
-          position: 'absolute',
-          left: 100,
-          top: 300,
-          width: 70,
-          height: 70,
-          'background-color': 'purple'
-        },
-        name: 'grape'
-      },
-      {
-        style: {
-          position: 'absolute',
-          left: 300,
-          top: 250,
-          width: 20,
-          height: 100,
-          'background-color': 'red'
-        },
-        name: 'gun'
-      }
-    ]
-  }
-});
+const store = createStore(reducers, initialState);
 
 render(
   <Provider store={store}>
---
+++ src/initialState.js
@@ -0,0 +1,52 @@
+export default {
+  player: {
+    items: [
+      'torch',
+      'key1',
+      'pot roast',
+      'key2',
+      'key3'
+    ]
+  },
+  menu: 'MAIN',
+  ui: {
+    page: 0
+  },
+  room: {
+    items: [
+      {
+        style: {
+          position: 'absolute',
+          left: 100,
+          top: 100,
+          width: 50,
+          height: 50,
+          'background-color': 'yellow'
+        },
+        name: 'bong'
+      },
+      {
+        style: {
+          position: 'absolute',
+          left: 100,
+          top: 300,
+          width: 70,
+          height: 70,
+          'background-color': 'purple'
+        },
+        name: 'grape'
+      },
+      {
+        style: {
+          position: 'absolute',
+          left: 300,
+          top: 250,
+          width: 20,
+          height: 100,
+          'background-color': 'red'
+        },
+        name: 'gun'
+      }
+    ]
+  }
+};
