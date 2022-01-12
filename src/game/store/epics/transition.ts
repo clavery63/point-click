78736f0adfa389b528a7f6e2584195a1902b3dfc
Chris Lavery <clavery63@gmail.com>
2022-01-12 00:54:34 -0500

    another file, another concssion

---
+++ src/game/store/epics/transition.ts
@@ -0,0 +1,85 @@
+import { mapTo, switchMap, mergeMap, concatMap, withLatestFrom, take, filter } from 'rxjs/operators';
+import { from, timer, concat, of, NEVER, Observable } from 'rxjs';
+import { range } from 'lodash';
+import { ofType } from 'redux-observable';
+import { AllActions, MyEpic } from './types';
+import { isOfType } from 'typesafe-actions';
+import { GameStoreState } from '../types';
+import { ActionsType } from '../reducers/rootReducer';
+
+const MS_PER_FRAME = 65;
+
+type When = (p: boolean, a: AllActions) => AllActions;
+const when: When = (pred, action) => {
+  return pred ? action : { type: 'NULL' };
+};
+
+const getAction$ = (dest: number) => (frame: number) => {
+  return from([
+    when(frame === 0, { type: 'SET_CURSOR_ENABLED', payload: false }),
+    when(frame === 14, { type: 'SET_CURSOR_ENABLED', payload: true }),
+    when(frame === 7, { type: 'SET_ROOM', payload: dest }),
+    when(true, { type: 'SET_FRAME', payload: frame % 14 }),
+    when(frame === 0 || frame === 7, { type: 'PLAY_SFX', payload: null })
+  ]);
+};
+
+type TextRunner = (t: string) => Observable<ActionsType['SET_TEXT']>;
+type DispatchRoom = (d: number, s: GameStoreState, r: TextRunner) => Observable<AllActions>;
+const dispatchRoom: DispatchRoom = (dest, state, runText$) => {
+  const { worldState, playerState } = state;
+  const room = worldState.rooms[dest];
+  if (!room.description) {
+    return of({ type: 'NULL' });
+  }
+
+  if (playerState.timer === 17) {
+    return of({ type: 'RUN_TRANSITION', payload: { dest: 11, dir: 'FORWARD', frame: 0 } });
+  }
+
+  return concat(
+    of(when(!!room.gameOver || !!room.music, { 
+      type: 'PLAY_MUSIC',
+      payload: { 
+        fileName: room.music || 'puppets.mp3'
+      }
+    })),
+    runText$(room.initialDescription || room.description)
+  );
+};
+
+type CheckGameOver = (d: number, s: GameStoreState, p: Observable<any>) => Observable<AllActions>
+const checkGameOver: CheckGameOver = (dest, state, pageClick$) => {
+  const { worldState } = state;
+  const room = worldState.rooms[dest];
+  if (!room.gameOver) {
+    return NEVER;
+  }
+
+  return pageClick$.pipe(
+    // TODO: yeah, you're reading this right. using that `when` function as a passthru
+    // Allows the typechecker to confirm this is valid. Find a better way? :(
+    mapTo(when(true, {
+      type: 'SET_MENU',
+      payload: 'GAME_OVER'
+    })),
+    take(1),
+  )
+};
+
+const transition$: MyEpic = (action$, state$, { runText$ }) => {
+  return action$.pipe(
+    filter(isOfType('RUN_TRANSITION')),
+    withLatestFrom(state$),
+    switchMap(([{ payload }, state]) => concat(
+      from(range(15)).pipe(
+        concatMap(frame => timer(MS_PER_FRAME).pipe(mapTo(frame))),
+        mergeMap(getAction$(payload.dest))
+      ),
+      from(dispatchRoom(payload.dest, state, runText$)),
+      checkGameOver(payload.dest, state, action$.pipe(ofType('PAGE_CLICK')))
+    ))
+  );
+};
+
+export default transition$;
--- src/game/store/epics/types.ts
+++ src/game/store/epics/types.ts
@@ -1,6 +1,6 @@
 import { Epic } from 'redux-observable';
 import { ActionsType, ReducerActions } from '../reducers/rootReducer';
-import { GameStoreState } from '../types';
+import { DoorDir, GameStoreState } from '../types';
 import { Observable } from 'rxjs';
 
 type EpicActionTypes = {
@@ -9,9 +9,15 @@ type EpicActionTypes = {
   LOAD_GAME: null,
   PLAY_SFX: null,
   PLAY_MUSIC: {
-    text: string,
+    text?: string,
     fileName: string
-  }
+  },
+  RUN_TRANSITION: {
+    dest: number,
+    dir: DoorDir,
+    frame: number
+  },
+  PAGE_CLICK: null
 };
 
 type EpicActionsType = {
@@ -23,10 +29,10 @@ type EpicActionsType = {
 
 type EpicActions = EpicActionsType[keyof EpicActionTypes];
 
-type AllActions = ReducerActions | EpicActions;
+export type AllActions = ReducerActions | EpicActions;
 
 type Dependencies = {
   runText$: (t: string) => Observable<ActionsType['SET_TEXT']>
 }
 
-export type MyEpic = Epic<AllActions, ReducerActions, GameStoreState, Dependencies>;
+export type MyEpic = Epic<AllActions, AllActions, GameStoreState, Dependencies>;
