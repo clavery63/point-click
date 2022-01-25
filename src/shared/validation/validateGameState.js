01dfef457aaa52e6b68d5d76955d949d30f87c6b
Chris Lavery <clavery63@gmail.com>
2022-01-24 23:08:12 -0500

    acutally use the generated ValuePaths for validation

--- src/shared/validation/validateGameState.js
+++ src/shared/validation/validateGameState.js
@@ -1,16 +1,35 @@
 import Ajv from 'ajv';
 import gameStateSchema from './generated/gameStateSchema';
+import { numberPaths } from './generated/validPaths';
+
+const escapeRegExp = source => {
+  return source.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+};
+
+const constructRegExp = type => {
+  if (type.value) {
+    return new RegExp(`^${type.value}$`);
+  }
+
+  const regExpString = type.texts
+    .map(escapeRegExp)
+    .join('\\d');
+
+  return new RegExp(`^${regExpString}$`);
+};
 
 const validateGameState = gameState => {
   const ajv = new Ajv();
+  const numberPathRegExes = numberPaths.map(constructRegExp);
 
   ajv.addKeyword({
     keyword: "ValuePath",
     type: "string",
     validate: (schema, data) => {
-      console.log('schema', schema);
-      console.log('data:', data);
-      return data === 'chris';
+      // TODO: at some point we might want to work for more than just number
+      // paths, at which point we will actually look at `schema` to pick the
+      // apropriate ValidPaths regex. phew!
+      return numberPathRegExes.some(regEx => data.match(regEx));
     },
     error: {
       message: 'you blew it'
