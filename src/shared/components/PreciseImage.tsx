98ae9a24a1833d524873be6e5685881d490fa8df
Chris Lavery <clavery63@gmail.com>
2022-02-10 22:17:51 -0500

    update cursor on hover

--- src/shared/components/PreciseImage.tsx
+++ src/shared/components/PreciseImage.tsx
@@ -1,9 +1,17 @@
 /* eslint-disable react/prop-types */
 
+import { KonvaEventObject } from 'konva/types/Node';
 import React, { useEffect, useState } from 'react';
 import { Image } from './tappables';
 
 type Props = React.ComponentPropsWithRef<typeof Image>;
+
+const setCursorStyle = (style: string) => (e: KonvaEventObject<MouseEvent>) => {
+  const container = e.target.getStage()?.container();
+  if (container) {
+    container.style.cursor = style;
+  }
+};
 
 const PreciseImage = (props: Props) => {
   const imgRef = React.useRef<any>(null); // Sorry.
@@ -19,7 +27,15 @@ const PreciseImage = (props: Props) => {
     setHack(props.image);
   }, [props.image]);
 
-  return <Image {...props} ref={imgRef} image={hack} />;
+  return (
+    <Image
+      {...props}
+      ref={imgRef}
+      image={hack}
+      onMouseEnter={setCursorStyle('pointer')}
+      onMouseLeave={setCursorStyle('default')}
+    />
+  );
 };
 
 export default PreciseImage;
