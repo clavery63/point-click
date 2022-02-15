9a7803af49261980c131276e6241a67cf1be974d
Chris Lavery <clavery63@gmail.com>
2022-02-15 01:02:35 -0500

    fix EntityDetails

--- src/admin/ui/rooms/EntityDetails.tsx
+++ src/admin/ui/rooms/EntityDetails.tsx
@@ -4,17 +4,17 @@ import ItemDetails from './ItemDetails';
 
 const EntityDetails = () => {
   const selectedEntity = useSelector(state => state.editorState.selectedEntity);
+  // TODO: combine these so we don't have to do the weird '-1' hack below
+  const entity = useSelector(state => {
+    return state.gameState.worldState.entities[selectedEntity?.id || -1];
+  });
 
-  if (!selectedEntity) {
+  if (!entity) {
     return null;
   }
 
-  const entity = useSelector(state => {
-    return state.gameState.worldState.entities[selectedEntity.id];
-  });
-
   if (entity.type === 'items') {
-    <ItemDetails item={entity} />;
+    return <ItemDetails item={entity} />;
   }
 
   // TODO: add scenery and doors
