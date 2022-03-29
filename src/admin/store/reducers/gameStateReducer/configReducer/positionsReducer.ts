b7af69946b16387a00dda4f246076bb21c7f119c
Chris Lavery <clavery63@gmail.com>
2022-03-29 01:57:31 -0400

    make verbs draggable

--- src/admin/store/reducers/gameStateReducer/configReducer/positionsReducer.ts
+++ src/admin/store/reducers/gameStateReducer/configReducer/positionsReducer.ts
@@ -1,14 +1,28 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { setGameState } from 'admin/store/sharedActions';
 import { PositionsConfig } from 'game/store/types';
+
+type VerbPayload = {
+  index: number;
+  top: number;
+  left: number;
+};
 
 export const positionsSlice = createSlice({
   name: 'positions',
   initialState: {} as PositionsConfig,
-  reducers: {},
+  reducers: {
+    setVerbPosition: (state, action: PayloadAction<VerbPayload>) => {
+      const { index, top, left } = action.payload;
+
+      state.verbs[index] = { top, left };
+    },
+  },
   extraReducers: builder => {
     builder.addCase(setGameState, (state, action) => action.payload.config.positions);
   },
 });
+
+export const { setVerbPosition } = positionsSlice.actions;
 
 export default positionsSlice.reducer;
--- src/admin/ui/config/gameLayout/VerbsUI.tsx
+++ src/admin/ui/config/gameLayout/VerbsUI.tsx
@@ -1,5 +1,8 @@
-import { useSelector } from 'admin/ui/hooks/redux';
+import { setVerbPosition } from 'admin/store/reducers/gameStateReducer/configReducer/positionsReducer';
+import { useSelector, useDispatch } from 'admin/ui/hooks/redux';
+import useCachebuster from 'admin/ui/hooks/useCachebuster';
 import { TextStateless as Text, makeCanvasSet } from 'game/ui/shared/Text';
+import { KonvaEventObject } from 'konva/types/Node';
 import React from 'react';
 import { Group, Image } from 'react-konva';
 import { createSelector } from 'reselect';
@@ -20,12 +23,26 @@ type Props = {
 };
 const Verb = (props: Props) => {
   const { top, left, index } = props;
+  const dispatch = useDispatch();
   const verbs = useSelector(state => state.gameState.config.verbs);
   const images = useSelector(state => state.gameState.images);
   const canvases = useSelector(canvasesSelector);
+  const cachebuster = useCachebuster(top + left);
 
   return (
-    <Group x={left} y={top} draggable onClick={() => console.log(verbs[index])}>
+    <Group
+      x={left + cachebuster}
+      y={top + cachebuster}
+      draggable
+      onClick={() => console.log(verbs[index])}
+      onDragEnd={(e: KonvaEventObject<DragEvent>) => {
+        dispatch(setVerbPosition({
+          top: Math.round(e.target.y()),
+          left: Math.round(e.target.x()),
+          index,
+        }));
+      }}
+    >
       <Image image={images['menu-button']} />
       <Text text={verbs[index].name} left={9} top={1} canvases={canvases} />
     </Group>
@@ -33,11 +50,11 @@ const Verb = (props: Props) => {
 };
 
 const VerbsUI = () => {
-  const { verbs } = useSelector(state => state.gameState.config.positions);
+  const { verbs = [] } = useSelector(state => state.gameState.config.positions);
   return (
     <Group>
       {verbs.map(({ left, top }, index) => (
-        <Verb left={left} top={top} index={index} />
+        <Verb left={left} top={top} index={index} key={index} />
       ))}
     </Group>
   );
