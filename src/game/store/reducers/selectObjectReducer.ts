19b404f27b74f1a2bbb47644bfd4b1c1baf8c596
Chris Lavery <clavery63@gmail.com>
2022-07-18 01:56:24 -0400

    remove hardcoded indexing of selectObjectReducer

--- src/game/store/reducers/selectObjectReducer.ts
+++ src/game/store/reducers/selectObjectReducer.ts
@@ -1,26 +1,31 @@
-import { ParentReducer } from 'shared/util/types';
+import { EntityReducer, ParentReducer } from 'shared/util/types';
 import moveReducer from './verbReducers/moveReducer';
 import openReducer from './verbReducers/openReducer';
 import takeReducer from './verbReducers/takeReducer';
 import _useReducer, { forfeitItemReducer } from './verbReducers/useReducer';
 import { keepState, withText } from './utils';
-import { VerbIndex, VerbConfig } from '../types';
+import {
+  VerbIndex, VerbConfig, Entity,
+} from '../types';
 import genericVerbReducer from './verbReducers/genericVerbReducer';
 
-const getReducer = (verb: VerbIndex, verbs: VerbConfig[]) => ([
-  // TODO: None of these behaviors should be hardcoded. Special behaviors like
-  // MOVE and TAKE should be user-selected in the verb config. In a way,
-  // defaultText is just another special behavior.
-  genericVerbReducer(0, moveReducer),
-  genericVerbReducer(1, object => withText(object.description)),
-  genericVerbReducer(2, openReducer),
-  genericVerbReducer(3, _useReducer, forfeitItemReducer),
-  genericVerbReducer(4, () => withText(verbs[4].defaultText)),
-  genericVerbReducer(5, takeReducer),
-  genericVerbReducer(6, () => withText(verbs[6].defaultText)),
-  genericVerbReducer(7, () => withText(verbs[7].defaultText)),
-  genericVerbReducer(8, () => withText(verbs[8].defaultText)),
-][verb]);
+const failLookup: { [key: string]: EntityReducer } = {
+  MOVE: moveReducer,
+  LOOK: (object: Entity) => withText(object.description),
+  OPEN: openReducer,
+  USE: _useReducer,
+  TAKE: takeReducer,
+};
+
+const successLookup: { [key: string]: EntityReducer } = {
+  USE: forfeitItemReducer,
+};
+
+const getReducer = (verb: VerbIndex, verbs: VerbConfig[]) => {
+  const { defaultBehavior, defaultText } = verbs[verb];
+  const failBehavior = failLookup[defaultBehavior] || (() => withText(defaultText));
+  return genericVerbReducer(verb, failBehavior, successLookup[defaultBehavior]);
+};
 
 type GenericReducer = (t: 'doors' | 'entities') => ParentReducer<number>;
 const selectObjectReducer: GenericReducer = entType => {
