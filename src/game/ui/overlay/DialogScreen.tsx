a48d94098814b99840653f32d7fe937265e0a0e4
Chris Lavery <clavery63@gmail.com>
2022-09-14 00:27:09 -0400

    get the dialog to scroll onto the screen

--- src/game/ui/overlay/DialogScreen.tsx
+++ src/game/ui/overlay/DialogScreen.tsx
@@ -1,9 +1,31 @@
 import { textToLines } from 'game/store/epics/util';
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Group, Rect } from 'react-konva';
+import { interval } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 import { Image } from 'shared/components/tappables';
 import { useSelector } from 'shared/hooks/redux';
 import Text from '../shared/Text';
+
+const frame$ = (numFrames: number) => interval(16).pipe(
+  takeWhile((frame: number) => frame <= numFrames),
+);
+
+type Dialog = { question: string; answers: string[] };
+const computeDialog = (input: Dialog, currentFrame: number) => {
+  const answersTruncated = input.answers.slice(0, input.answers.length - 1);
+  const startFrames = answersTruncated.reduce((acc, cur) => {
+    acc.push(acc[acc.length - 1] + cur.length);
+    return acc;
+  }, [input.question.length]);
+
+  return {
+    question: input.question.slice(0, currentFrame),
+    answers: startFrames.map((startFrame, index) => {
+      return input.answers[index].slice(0, Math.max(currentFrame - startFrame, 0));
+    }),
+  };
+};
 
 const Avatar = () => {
   return (
@@ -32,15 +54,29 @@ const Answer = ({ answer, index }: AnswerProps) => {
 };
 
 const DialogScreen = () => {
-  const question = "well why don't we put a couple of lines of text here. OK, it seems like 4 lines is the max.";
-  const questionLines = textToLines(23)(question);
+  const inputDialog = useMemo(() => ({
+    question: "well why don't we put a couple of lines of text here. OK, it seems like 4 lines is the max.",
+    answers: [
+      "I don't know. why don't you tell me?",
+      'I mean, as far as I can tell it looks fine',
+      'No clue.',
+      'Does the extra space here look awkward?',
+    ],
+  }), []);
 
-  const answers = [
-    "I don't know. why don't you tell me?",
-    'I mean, as far as I can tell it looks fine',
-    'No clue.',
-    'Does the extra space here look awkward?',
-  ];
+  const [currentFrame, setCurrentFrame] = useState(0);
+
+  useEffect(() => {
+    const { question, answers } = inputDialog;
+    const totalLength = question.length + answers.join('').length;
+    const subscription = frame$(totalLength).subscribe(setCurrentFrame);
+
+    return () => subscription.unsubscribe();
+  }, [inputDialog]);
+
+  const { question, answers } = computeDialog(inputDialog, currentFrame);
+
+  const questionLines = textToLines(23)(question);
 
   return (
     <Group
